2fe641bd83bc9fe2bad7870f8170e21ca9cd211a
samir20-23 <germanysamir1@gmail.com>
2024-10-14 15:47:14 +0100

    new

--- tutors/tuto_3.kt
+++ tutors/tuto_3.kt
@@ -1,2 +1,20 @@
 package com.example.fundamentals_KOTLIN.tutors
 
+
+class Class1 {
+    init{
+        println("class2");
+    }
+}
+class Class2{
+    fun play(){
+        println("class1");
+    }
+}
+
+//
+fun main () {
+    Class1();
+    val  myclass = Class2();
+    myclass.play();
+}
